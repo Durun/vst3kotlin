28b91ca7e5a38551180ebbbb58b0450d4284e072
Durun <negiigainuki@gmail.com>
2021-02-08 06:22:16 +0900

    add UnixPath and test

---
+++ interop/src/commonMain/kotlin/io/github/durun/path/UnixPath.kt
@@ -0,0 +1,61 @@
+package io.github.durun.path
+
+class UnixPath
+private constructor(
+	override val isAbsolute: Boolean,
+	private val elements: List<String>
+) : Path {
+	companion object {
+		val fileSystem: FileSystem = FileSystem.Unix
+		fun of(path: String): UnixPath {
+			require(path.isValidPath()) { "invalid path: $path" }
+			return UnixPath(
+				isAbsolute = path.isAbsolutePath(),
+				elements = path.splitToElements()
+			)
+		}
+
+		private val invalidCharacters = listOf("\n", "\r", "\b")
+		private fun String.isAbsolutePath() = this.startsWith("/")
+		private fun String.splitToElements() = this.split("/").filterNot { it.isEmpty() }
+		private fun String.isValidPath() = invalidCharacters.all { !this.contains(it) } && !this.contains("//")
+	}
+
+	override fun resolve(other: String): Path {
+		return when {
+			isRoot() -> of("/$other")
+			else -> of("$this/$other")
+		}
+	}
+
+	override fun resolve(other: Path): Path {
+		return when {
+			isRoot() -> of("/$other")
+			else -> of("$this/$other")
+		}
+	}
+
+	override fun toString(): String {
+		return (if (isAbsolute) "/" else "") + elements.joinToString("/")
+	}
+
+	override fun equals(other: Any?): Boolean {
+		if (this === other) return true
+		if (other == null || this::class != other::class) return false
+
+		other as UnixPath
+
+		if (isAbsolute != other.isAbsolute) return false
+		if (elements != other.elements) return false
+
+		return true
+	}
+
+	override fun hashCode(): Int {
+		var result = isAbsolute.hashCode()
+		result = 31 * result + elements.hashCode()
+		return result
+	}
+
+	private fun isRoot() = elements.isEmpty()
+}
---
+++ interop/src/commonTest/kotlin/io/github/durun/path/UnixPathTest.kt
@@ -0,0 +1,55 @@
+package io.github.durun.path
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kotlin.test.Test
+
+
+class UnixPathTest {
+	@Test
+	fun of() {
+		shouldThrow<Exception> { UnixPath.of("double//slash") }
+		shouldThrow<Exception> { UnixPath.of("invalid/\nnewline") }
+		shouldThrow<Exception> { UnixPath.of("invalid/\rreturn") }
+		shouldThrow<Exception> { UnixPath.of("invalid/\b") }
+	}
+
+	@Test
+	fun isAbsolute() {
+		UnixPath.of("/").isAbsolute shouldBe true
+		UnixPath.of("/home/user").isRelative shouldBe false
+		UnixPath.of(".").isRelative shouldBe true
+		UnixPath.of("../folder/").isAbsolute shouldBe false
+	}
+
+	@Test
+	fun resolve_String() {
+		UnixPath.of("/").resolve("home/user") shouldBe UnixPath.of("/home/user")
+		UnixPath.of("user").resolve("bin") shouldBe UnixPath.of("user/bin")
+		shouldThrow<Exception> { UnixPath.of("/").resolve("/absolute/path") }
+		shouldThrow<Exception> { UnixPath.of("/").resolve("path/with/\n/invalid/char") }
+		shouldThrow<Exception> { UnixPath.of("/").resolve("path/with/double//slash/") }
+	}
+
+	@Test
+	fun resolve_Path() {
+		UnixPath.of("/").resolve(UnixPath.of("home/user")) shouldBe UnixPath.of("/home/user")
+		UnixPath.of("user").resolve(UnixPath.of("bin")) shouldBe UnixPath.of("user/bin")
+		shouldThrow<Exception> { UnixPath.of("/").resolve(UnixPath.of("/absolute/path")) }
+	}
+
+	@Test
+	fun testToString() {
+		UnixPath.of("/").toString() shouldBe "/"
+		UnixPath.of("/home/user/").toString() shouldBe "/home/user"
+		UnixPath.of(".").toString() shouldBe "."
+		UnixPath.of("../folder/").toString() shouldBe "../folder"
+	}
+
+	@Test
+	fun testEquals() {
+		UnixPath.of("/home/user/") shouldBe UnixPath.of("/home/user")
+		UnixPath.of("home/user/") shouldNotBe UnixPath.of("/home/user/")
+	}
+}
