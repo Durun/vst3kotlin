d35f6f8c7d3eb27416e6e3f2d4e1f3be9e2c4265
Durun <negiigainuki@gmail.com>
2021-02-10 04:28:15 +0900

    add cwrapper IPluginFactory2,3

--- vst3cwrapper/src/main/public/PClassInfo.h
+++ vst3cwrapper/src/main/public/PClassInfo.h
@@ -17,6 +17,7 @@ enum {
 	PClassInfo_kNameSize = 64
 };
 
+// version 1
 typedef struct PClassInfo {
 	TUID cid;
 	int32 cardinality;
@@ -24,6 +25,38 @@ typedef struct PClassInfo {
 	char8 name[PClassInfo_kNameSize];
 } PClassInfo;
 
+// version 2
+enum {
+	kVendorSize = 64,
+	kVersionSize = 64,
+	kSubCategoriesSize = 128
+};
+typedef struct PClassInfo2 {
+	TUID cid;
+	int32 cardinality;
+	char8 category[PClassInfo_kCategorySize];
+	char8 name[PClassInfo_kNameSize];
+	// version 2
+	uint32 classFlags;
+	char8 subCategories[kSubCategoriesSize];
+	char8 vendor[kVendorSize];
+	char8 version[kVersionSize];
+	char8 sdkVersion[kVersionSize];
+} PClassInfo2;
+
+// Unicode version
+typedef struct PClassInfoW {
+	TUID cid;
+	int32 cardinality;
+	char8 category[PClassInfo_kCategorySize];
+	char16 name[PClassInfo_kNameSize];
+	// version 2
+	uint32 classFlags;
+	char8 subCategories[kSubCategoriesSize];
+	char16 vendor[kVendorSize];
+	char16 version[kVersionSize];
+	char16 sdkVersion[kVersionSize];
+} PClassInfoW;
 
 #ifdef __cplusplus
 }
