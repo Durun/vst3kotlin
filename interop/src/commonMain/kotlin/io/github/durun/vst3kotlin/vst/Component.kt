4b2f42bb7a990e231bca9b07b2ba62c0e2f0f0b7
Durun <negiigainuki@gmail.com>
2021-02-17 00:31:45 +0900

    add component info classes

--- interop/src/commonMain/kotlin/io/github/durun/vst3kotlin/vst/Component.kt
+++
@@ -1,8 +0,0 @@
-package io.github.durun.vst3kotlin.vst
-
-import io.github.durun.vst3kotlin.base.PluginBase
-import io.github.durun.vst3kotlin.base.UID
-
-expect class Component : PluginBase {
-	val controllerClassID: UID
-}
