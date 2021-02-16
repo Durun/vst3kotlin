1426373763cd8521adf788ff0027d638f2542ac5
Durun <negiigainuki@gmail.com>
2021-02-16 17:04:54 +0900

    add TUID.bytes

--- interop/src/commonMain/kotlin/io/github/durun/vst3kotlin/base/TUID.kt
+++ interop/src/commonMain/kotlin/io/github/durun/vst3kotlin/base/TUID.kt
@@ -4,7 +4,7 @@ import io.github.durun.util.decodeAsBigEndian
 import io.github.durun.util.encodeBigEndian
 
 data class TUID(
-	private val bytes: ByteArray
+	val bytes: ByteArray
 ) {
 	constructor(id: String) : this(id.decodeAsBigEndian())
 
