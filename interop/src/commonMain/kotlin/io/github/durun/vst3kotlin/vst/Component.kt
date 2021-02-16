5ed706e63ca8f5384cb379586b363b2d2ebdafee
Durun <negiigainuki@gmail.com>
2021-02-16 21:07:59 +0900

    add Component.controllerClassID

--- interop/src/commonMain/kotlin/io/github/durun/vst3kotlin/vst/Component.kt
+++ interop/src/commonMain/kotlin/io/github/durun/vst3kotlin/vst/Component.kt
@@ -1,6 +1,8 @@
 package io.github.durun.vst3kotlin.vst
 
 import io.github.durun.vst3kotlin.base.PluginBase
+import io.github.durun.vst3kotlin.base.UID
 
 expect class Component : PluginBase {
+	val controllerClassID: UID
 }
--- interop/src/linuxX64Main/kotlin/io/github/durun/vst3kotlin/vst/Component.kt
+++ interop/src/linuxX64Main/kotlin/io/github/durun/vst3kotlin/vst/Component.kt
@@ -1,10 +1,21 @@
 package io.github.durun.vst3kotlin.vst
 
-import cwrapper.IAudioProcessor
+import cwrapper.IComponent
+import cwrapper.IComponent_getControllerClassId
 import io.github.durun.vst3kotlin.base.PluginBase
-import kotlinx.cinterop.CPointer
+import io.github.durun.vst3kotlin.base.UID
+import io.github.durun.vst3kotlin.base.toUID
+import kotlinx.cinterop.*
 
 actual class Component(
-	thisPtr: CPointer<IAudioProcessor>
+	thisPtr: CPointer<IComponent>
 ) : PluginBase(thisPtr) {
+	private val thisPtr get() = thisRawPtr.reinterpret<IComponent>()
+	actual val controllerClassID: UID by lazy {
+		memScoped {
+			val tuid = allocArray<ByteVar>(16)
+			val result = IComponent_getControllerClassId(thisPtr, tuid)
+			tuid.toUID()
+		}
+	}
 }
