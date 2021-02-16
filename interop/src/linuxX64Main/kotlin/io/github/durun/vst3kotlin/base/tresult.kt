9fc115814074d48c942db7c26b4d11f3b87de9ee
Durun <negiigainuki@gmail.com>
2021-02-16 17:03:31 +0900

    add kResultString

---
+++ interop/src/linuxX64Main/kotlin/io/github/durun/vst3kotlin/base/tresult.kt
@@ -0,0 +1,17 @@
+package io.github.durun.vst3kotlin.base
+
+import cwrapper.*
+
+val Int.kResultString: String
+	get() = when(this) {
+		kResultOk -> "kResultOK"
+		kResultTrue -> "kResultTrue"
+		kResultFalse -> "kResultFalse"
+		kNoInterface -> "kNoInterface"
+		kInvalidArgument -> "kInvalidArgument"
+		kNotImplemented -> "kNotImplemented"
+		kInternalError -> "kInternalError"
+		kNotInitialized -> "kNotInitialized"
+		kOutOfMemory -> "kOutOfMemory"
+		else -> "unknown"
+	}
