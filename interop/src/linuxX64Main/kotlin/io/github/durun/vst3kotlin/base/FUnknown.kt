e89b81c4bd700060dcf9da3bae582e2942ea581a
Durun <negiigainuki@gmail.com>
2021-02-17 13:47:12 +0900

    add util FUnknown.queryInterface

--- interop/src/linuxX64Main/kotlin/io/github/durun/vst3kotlin/base/FUnknown.kt
+++ interop/src/linuxX64Main/kotlin/io/github/durun/vst3kotlin/base/FUnknown.kt
@@ -1,9 +1,12 @@
 package io.github.durun.vst3kotlin.base
 
+import cwrapper.FUnknown_queryInterface
 import cwrapper.FUnknown_release
+import cwrapper.IProcessContextRequirements
+import cwrapper.kResultTrue
 import io.github.durun.io.Closeable
-import kotlinx.cinterop.CPointer
-import kotlinx.cinterop.reinterpret
+import io.github.durun.vst3kotlin.VstInterface
+import kotlinx.cinterop.*
 
 actual abstract class FUnknown(
 	protected val thisRawPtr: CPointer<*>
@@ -16,5 +19,23 @@ actual abstract class FUnknown(
 		check(isOpen)
 		FUnknown_release(thisPtr)
 		isOpen = false
+	}
+
+	private fun queryInterface(interfaceID: UID): CPointer<*> {
+		return memScoped {
+			val iid = interfaceID.toFuidPtr(this).pointed.tuid
+			val obj = alloc<CPointerVar<*>>()
+			val result = FUnknown_queryInterface(thisPtr, iid, obj.ptr)
+			check(result == kResultTrue) { result.kResultString }
+			val ptr = obj.value
+			checkNotNull(ptr)
+			ptr
+		}
+	}
+
+	fun <I : CPointed> queryInterface(): VstInterface<CPointer<I>> {
+		val iid = VstInterface.IID.get<IProcessContextRequirements>()
+		val ptr = queryInterface(iid).reinterpret<I>()
+		return VstInterface(ptr)
 	}
 }
