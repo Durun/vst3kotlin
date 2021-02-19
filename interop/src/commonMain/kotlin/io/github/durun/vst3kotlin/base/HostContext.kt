1e7503c662bc8d9ae37bc753cbe6cd9cb3ebd37f
Durun <negiigainuki@gmail.com>
2021-02-19 17:53:47 +0900

    fix

--- interop/src/commonMain/kotlin/io/github/durun/vst3kotlin/base/HostContext.kt
+++ interop/src/commonMain/kotlin/io/github/durun/vst3kotlin/base/HostContext.kt
@@ -1,5 +1,6 @@
 package io.github.durun.vst3kotlin.base
 
+import io.github.durun.util.ByteArrayBuilder
 import io.github.durun.util.ByteArrayReader
 import io.github.durun.util.MessageBase
 import io.github.durun.util.buildByteArray
@@ -12,11 +13,18 @@ sealed class Message(reader: ByteArrayReader) : MessageBase(reader) {
 	class BeginEdit(reader: ByteArrayReader) : Message(reader) {
 		override val type: Int = typeBeginEdit
 		val id: UInt by uInt()
+		override fun toString(): String = "BeginEdit(id=$id)"
 
 		companion object {
-			fun of(id: UInt): BeginEdit = BeginEdit(ByteArrayReader(bytesOf(id)))
-			fun bytesOf(id: UInt): ByteArray = buildByteArray {
+			private fun ByteArrayBuilder.bodyBytes(id: UInt) {
 				appendUInt(id)
+			}
+			fun bytesOf(id: UInt) :ByteArray = buildByteArray{
+				appendInt(typeBeginEdit)
+				bodyBytes(id)
+			}
+			fun of(id: UInt): BeginEdit {
+				return BeginEdit(ByteArrayReader(ByteArrayBuilder().apply { bodyBytes(id) }.build()))
 			}
 		}
 	}
@@ -38,7 +46,9 @@ sealed class Message(reader: ByteArrayReader) : MessageBase(reader) {
 		@OptIn(ExperimentalStdlibApi::class)
 		inline fun <reified T : MessageBase> decodeAll(reader: ByteArrayReader, sizeByte: Int): List<T> {
 			return buildList {
-				while (reader.offset < sizeByte) add(decode(reader))
+				while (reader.offset < sizeByte) {
+					add(decode(reader))
+				}
 			}
 		}
 	}
