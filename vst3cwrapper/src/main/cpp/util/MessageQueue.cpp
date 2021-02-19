9b4b196309feaaa63d98cdab69dc21c66ce36f05
Durun <negiigainuki@gmail.com>
2021-02-19 23:17:50 +0900

    refactor

--- vst3cwrapper/src/main/cpp/util/MessageQueue.cpp
+++ vst3cwrapper/src/main/cpp/util/MessageQueue.cpp
@@ -3,58 +3,15 @@
 #include <cstdio>
 #include <cstdlib>
 
-#include "lock.h"
-
-static void enter_lock() {
-    Lock_enter(&(messageQueue.locked));
-}
-
-static void exit_lock() {
-    Lock_exit(&(messageQueue.locked));
-}
-
-static int min(int x, int y) {
-    if (x <= y)
-        return x;
-    else
-        return y;
-}
+#include "util/ByteQueue.h"
 
 void MessageQueue_init() {
-    messageQueue.nextReadIndex = 0;
-    messageQueue.nextWriteIndex = 0;
-    messageQueue.remainSize = 0;
-    messageQueue.locked = UNLOCK;
+    ByteQueue_init(&messageQueue);
     messageQueue.array = messageQueueArray;
 }
 
 void MessageQueue_enqueue(const char data[], int size) {
-    // check
-    if (size == 0) return;
-    if (size < 0) {
-        fprintf(stderr, "Illegal argument: size must be 0<= but %d\n", size);
-        exit(1);
-    }
-
-    // overflow check
-    enter_lock();
-    if (MessageQueueLength < messageQueue.remainSize + size) {
-        fprintf(stderr, "Illegal state: MessageQueue overflow\n");
-        exit_lock();
-        exit(1);
-    }
-
-    // copy
-    for (int i = 0; i < size; i++) {
-        int to = (messageQueue.nextWriteIndex + i) % MessageQueueLength;
-        messageQueue.array[to] = data[i];
-        //fprintf(stderr, "enqueue: %02d <- %c\n", to, data[i]);
-    }
-
-    // increment
-    messageQueue.nextWriteIndex = (messageQueue.nextWriteIndex + size) % MessageQueueLength;
-    messageQueue.remainSize += size;
-    exit_lock();
+    ByteQueue_enqueue(&messageQueue, data, size);
 }
 
 void MessageQueue_enqueue_byte(const char data) {
@@ -62,25 +19,5 @@ void MessageQueue_enqueue_byte(const char data) {
 }
 
 int MessageQueue_dequeue(/*out*/ char data[], const int readSize) {
-    // check
-    if (readSize < 0) {
-        fprintf(stderr, "Illegal argument: readSize must be 0<= but %d\n", readSize);
-        exit(1);
-    }
-
-    enter_lock();
-
-    // copy
-    auto toRead = min(readSize, messageQueue.remainSize);
-    for (int i = 0; i < toRead; i++) {
-        int from = (messageQueue.nextReadIndex + i) % MessageQueueLength;
-        data[i] = messageQueue.array[from];
-        //fprintf(stderr, "Dequeue: %02d -> %c\n", from, data[i]);
-    }
-
-    // increment
-    messageQueue.nextReadIndex = (messageQueue.nextReadIndex + toRead) % MessageQueueLength;
-    messageQueue.remainSize -= toRead;
-    exit_lock();
-    return toRead;
+    return ByteQueue_dequeue(&messageQueue, data, readSize);
 }
--- vst3cwrapper/src/main/public/util/MessageQueue.h
+++ vst3cwrapper/src/main/public/util/MessageQueue.h
@@ -1,23 +1,15 @@
 #pragma once
 #include "lock.h"
+#include "util/ByteQueue.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
 const int MessageQueueLength = 256; // 4KB
 
-typedef struct MessageQueue {
-    int locked;  // 0=unlock, 1=lock
-    char *array;
-    int nextReadIndex;
-    int nextWriteIndex;
-    int remainSize;
-} MessageQueue;
-
 static char messageQueueArray[MessageQueueLength] = {};
-static MessageQueue messageQueue = {UNLOCK, messageQueueArray};
+static ByteQueue messageQueue = {UNLOCK, messageQueueArray};
 
 void MessageQueue_init();
 void MessageQueue_enqueue(const char data[], int size);
