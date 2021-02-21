59d2dd41aa6dfe2c0fbf01d62f2e4a6f0074b34c
Durun <negiigainuki@gmail.com>
2021-02-21 22:09:32 +0900

    add download sample task for test

--- interop/build.gradle.kts
+++ interop/build.gradle.kts
@@ -1,9 +1,12 @@
+import de.undercouch.gradle.tasks.download.Download
+import de.undercouch.gradle.tasks.download.Verify
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithHostTests
 import org.jetbrains.kotlin.gradle.tasks.CInteropProcess
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("multiplatform") version "1.4.30"
+    id("de.undercouch.download") version "4.1.1"
 }
 
 repositories {
@@ -79,6 +82,34 @@ kotlin {
     linuxX64 { configureTarget() }
     macosX64 { configureTarget() }
     mingwX64("windowsX64") { configureTarget() }
+}
 
-
+// for testing
+tasks {
+    val zipPath = buildDir.resolve("vst3-samples-linux.zip")
+    val downloadSamplesLinux by creating(Download::class) {
+        src("https://github.com/Durun/vst3experiment/releases/download/samples/vst3-samples-linux.zip")
+        dest(zipPath)
+    }
+    val verifySamplesLinux by creating(Verify::class) {
+        dependsOn(downloadSamplesLinux)
+        src(zipPath)
+        algorithm("MD5")
+        checksum("7e1da79fc15fe53ce06aff13464f5c08")
+    }
+    val unzipSamplesLinux by creating(Copy::class) {
+        dependsOn(verifySamplesLinux)
+        from(zipTree(zipPath))
+        val destRoot = projectDir.resolve("src/commonTest/resources/vst3")
+        into(destRoot)
+        eachFile {
+            val dest =
+                if (2 < relativePath.segments.size) relativePath.segments.drop(2).joinToString("/")
+                else relativePath.pathString
+            copyTo(destRoot.resolve(dest))
+        }
+    }
+    getByName("linuxX64Test") {
+        dependsOn(unzipSamplesLinux)
+    }
 }
