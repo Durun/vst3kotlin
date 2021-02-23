3a728cfb15811d6019b94455082d0e1aacd79989
Durun <NegiIgaiNuki@gmail.com>
2021-02-23 21:18:48 +0900

    merge expect,actual declaration

--- interop/src/nativeMain/kotlin/io/github/durun/vst3kotlin/vst/ConnectionPoint.kt
+++ interop/src/nativeMain/kotlin/io/github/durun/vst3kotlin/vst/ConnectionPoint.kt
@@ -7,20 +7,20 @@ import io.github.durun.vst3kotlin.base.kResultString
 import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.reinterpret
 
-actual class ConnectionPoint(thisPtr: CPointer<IConnectionPoint>) : FUnknown(thisPtr), CClass {
+class ConnectionPoint(thisPtr: CPointer<IConnectionPoint>) : FUnknown(thisPtr), CClass {
 	override val ptr: CPointer<IConnectionPoint> get() = thisRawPtr.reinterpret()
 
-	actual fun connect(other: ConnectionPoint) {
+	fun connect(other: ConnectionPoint) {
 		val result = IConnectionPoint_connect(this.ptr, other.ptr)
 		check(result == kResultOk) { result.kResultString }
 	}
 
-	actual fun disconnect(other: ConnectionPoint) {
+	fun disconnect(other: ConnectionPoint) {
 		val result = IConnectionPoint_disconnect(this.ptr, other.ptr)
 		check(result == kResultOk) { result.kResultString }
 	}
 
-	actual fun notify(message: VstMessage) {
+	fun notify(message: VstMessage) {
 		val result = IConnectionPoint_notify(this.ptr, message.ptr)
 		check(result == kResultOk) { result.kResultString }
 	}
