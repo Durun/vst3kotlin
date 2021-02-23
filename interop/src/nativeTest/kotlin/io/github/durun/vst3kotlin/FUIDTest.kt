589b6cdc25d015ca9ade5f7b56db349bf1d62c42
Durun <NegiIgaiNuki@gmail.com>
2021-02-23 16:03:37 +0900

    fix toFuidPtr() for windows

--- interop/src/nativeTest/kotlin/io/github/durun/vst3kotlin/FUIDTest.kt
+++ interop/src/nativeTest/kotlin/io/github/durun/vst3kotlin/FUIDTest.kt
@@ -5,6 +5,7 @@ import cwrapper.FUID_from4Int
 import cwrapper.FUID_fromTUID
 import cwrapper.TUID_from4Int
 import io.github.durun.vst3kotlin.base.UID
+import io.github.durun.vst3kotlin.base.toFuidPtr
 import io.github.durun.vst3kotlin.base.toUID
 import io.kotest.matchers.shouldBe
 import kotlinx.cinterop.*
@@ -31,5 +32,15 @@ class FUIDTest {
 			fuid.tuid.toUID()
 		}
 		idFromC shouldBe UID("0123456789ABCDEF0123456789ABCDEF")
+	}
+
+	@Test
+	fun toFuidPtr() {
+		val uid = UID("0123456789ABCDEF0123456789ABCDEF")
+		memScoped {
+			val fuid = alloc<FUID>()
+			FUID_from4Int(fuid.ptr, 0x01234567, 0x89ABCDEF, 0x01234567, 0x89ABCDEF)
+			uid.toFuidPtr(this).readBytes(24) shouldBe fuid.ptr.readBytes(24)
+		}
 	}
 }
--- interop/src/windowsX64Main/kotlin/io/github/durun/vst3kotlin/base/uid.kt
+++ interop/src/windowsX64Main/kotlin/io/github/durun/vst3kotlin/base/uid.kt
@@ -1,5 +1,6 @@
 package io.github.durun.vst3kotlin.base
 
+import cwrapper.FUID
 import cwrapper.TUID
 import kotlinx.cinterop.*
 
@@ -15,3 +16,24 @@ fun TUID.toUID(): UID {
     return UID(component1 + component2 + component3 + remain)
 }
 
+fun UID.toFuidPtr(scope: AutofreeScope): CPointer<FUID> {
+    val fuid = cValue<FUID>().getPointer(scope)
+    val tuid = fuid.pointed.tuid
+    tuid[0]= bytes[3]
+    tuid[1]= bytes[2]
+    tuid[2]= bytes[1]
+    tuid[3]= bytes[0]
+    tuid[4]= bytes[5]
+    tuid[5]= bytes[4]
+    tuid[6]= bytes[7]
+    tuid[7]= bytes[6]
+    tuid[8] = bytes[8]
+    tuid[9] = bytes[9]
+    tuid[10] = bytes[10]
+    tuid[11] = bytes[11]
+    tuid[12] = bytes[12]
+    tuid[13] = bytes[13]
+    tuid[14] = bytes[14]
+    tuid[15] = bytes[15]
+    return fuid
+}
