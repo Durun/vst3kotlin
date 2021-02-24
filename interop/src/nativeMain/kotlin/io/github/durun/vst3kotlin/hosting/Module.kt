cff6544aa5276c99b221ac8258b4c3810356bb34
Durun <NegiIgaiNuki@gmail.com>
2021-02-24 22:31:30 +0900

    implement Module.kt

--- interop/src/nativeMain/kotlin/io/github/durun/vst3kotlin/hosting/Module.kt
+++ interop/src/nativeMain/kotlin/io/github/durun/vst3kotlin/hosting/Module.kt
@@ -1,13 +1,64 @@
 package io.github.durun.vst3kotlin.hosting
 
+import cwrapper.IPluginFactory
+import io.github.durun.dylib.Dylib
+import io.github.durun.io.Closeable
 import io.github.durun.path.Path
 import io.github.durun.vst3kotlin.base.PluginFactory
+import kotlinx.cinterop.CFunction
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.invoke
 
-expect class Module {
+expect object ModuleUtil {
+    fun moduleNameOf(vst3Path: Path): String
+    fun libPathOf(vst3Path: Path): Path
+    fun entryFuncOf(lib: Dylib): CPointer<CFunction<() -> Unit>>
+    fun exitFuncOf(lib: Dylib): CPointer<CFunction<() -> Unit>>
+    fun factoryGetterOf(lib: Dylib): CPointer<CFunction<() -> CPointer<IPluginFactory>>>
+}
+
+class Module
+private constructor(
+    val libPath: Path,
+    private val lib: Dylib
+) : Closeable {
     companion object {
-        fun of(modulePath: Path): Module
+        fun of(path: Path): Module {
+            val (libPath, lib) = runCatching {
+                val libPath = ModuleUtil.libPathOf(path)
+                libPath to Dylib.open(libPath)
+            }.recoverCatching {
+                path to Dylib.open(path)
+            }.getOrThrow()
+            return Module(libPath, lib)
+        }
     }
-    val name:String
-    val path:Path
-    val factory:PluginFactory
+
+    val name: String = lib.name
+    val factory: PluginFactory
+
+    override var isOpen: Boolean = true
+        private set
+
+    init {
+        // Initialize module
+        runCatching {
+            ModuleUtil.entryFuncOf(lib).invoke()
+        }.onFailure {
+            throw Exception("Failed to call entry function", it)
+        }
+        // Get Plugin factory
+        val factoryPtr = runCatching {
+            ModuleUtil.factoryGetterOf(lib).invoke()
+        }.onFailure {
+            throw Exception("Failed to call 'GetPluginFactory'", it)
+        }.getOrThrow()
+        factory = PluginFactory(factoryPtr)
+    }
+
+    override fun close() {
+        isOpen = false
+        ModuleUtil.exitFuncOf(lib)
+        lib.close()
+    }
 }
--- interop/src/windowsX64Main/kotlin/io/github/durun/vst3kotlin/hosting/Module.kt
+++ interop/src/windowsX64Main/kotlin/io/github/durun/vst3kotlin/hosting/Module.kt
@@ -1,14 +1,37 @@
 package io.github.durun.vst3kotlin.hosting
 
+import cwrapper.IPluginFactory
+import io.github.durun.dylib.Dylib
 import io.github.durun.path.Path
-import io.github.durun.vst3kotlin.base.PluginFactory
+import io.github.durun.path.exists
+import kotlinx.cinterop.CFunction
+import kotlinx.cinterop.CPointer
 
-actual class Module {
-    actual companion object {
-        actual fun of(modulePath: Path): Module = TODO()
+actual object ModuleUtil {
+    actual fun moduleNameOf(vst3Path: Path): String {
+        return vst3Path.nameWithoutExtension
     }
 
-    actual val path: Path = TODO()
-    actual val name: String = TODO()
-    actual val factory: PluginFactory = TODO()
+    actual fun libPathOf(vst3Path: Path): Path {
+        val name = vst3Path.nameWithoutExtension
+        return vst3Path.resolve("Contents/x86_64-win/$name.vst3").takeIf { it.exists() }
+            ?: vst3Path.takeIf { it.exists() }
+            ?: throw NoSuchElementException("Not found file: $vst3Path")
+    }
+
+    private const val entryName: String = "InitDll"
+    private const val exitName: String = "ExitDll"
+    private const val factoryGetterName: String = "GetPluginFactory"
+
+    actual fun entryFuncOf(lib: Dylib): CPointer<CFunction<() -> Unit>> {
+        return lib.getFunction(entryName)
+    }
+
+    actual fun exitFuncOf(lib: Dylib): CPointer<CFunction<() -> Unit>> {
+        return lib.getFunction(exitName)
+    }
+
+    actual fun factoryGetterOf(lib: Dylib): CPointer<CFunction<() -> CPointer<IPluginFactory>>> {
+        return lib.getFunction(factoryGetterName)
+    }
 }
