cee0cfab67f1eda7ce32db1f56ed39208d78d5bd
Durun <NegiIgaiNuki@gmail.com>
2021-02-24 19:40:49 +0900

    move RefCounted to common

--- interop/src/nativeMain/kotlin/io/github/durun/io/RefCounted.kt
+++ interop/src/nativeMain/kotlin/io/github/durun/io/RefCounted.kt
@@ -2,34 +2,36 @@ package io.github.durun.io
 
 import kotlin.native.concurrent.AtomicInt
 
-class RefCounted<T>(
+actual class RefCounted<T>
+actual constructor(
     private val onOpen: () -> T,
     private val onClose: (T) -> Unit
 ) {
     private var refCount: AtomicInt = AtomicInt(0)
-    var value: T? = null
-        private set
+    private var _value: T? = null
 
-    fun open(): T {
+    actual val value: T? get() = _value
+
+    actual fun open(): T {
         val count = refCount.addAndGet(1)
         return value ?: run {   // if not open yet
             check(count == 1)
             val newValue = onOpen()
-            value = newValue
+            _value = newValue
             newValue
         }
     }
 
-    fun close() {
+    actual fun close() {
         val count = refCount.addAndGet(-1)
         check(0 <= count) { "already closed" }
         if (count == 0) {
             onClose(value ?: throw Exception("Internal error: reference is null"))
-            value = null
+            _value = null
         }
     }
 
-    fun <R> use(block: (T) -> R): R {
+    actual fun <R> use(block: (T) -> R): R {
         val value = open()
         val result = runCatching { block(value) }
         close()
