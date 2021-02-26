fac2411eb854722ac9155baea071a2cd7579ff8d
Durun <NegiIgaiNuki@gmail.com>
2021-02-26 21:27:09 +0900

    disable window test

--- interop/src/windowsX64Test/kotlin/io/github/durun/vst3kotlin/windowTest.kt
+++ interop/src/windowsX64Test/kotlin/io/github/durun/vst3kotlin/windowTest.kt
@@ -1,20 +1,18 @@
 package io.github.durun.vst3kotlin
 
-import cwrapper.kPlatformTypeHWND
 import io.github.durun.io.use
 import io.github.durun.path.Path
 import io.github.durun.vst3kotlin.base.VstClassCategory
 import io.github.durun.vst3kotlin.cppinterface.HostCallback
 import io.github.durun.vst3kotlin.gui.PlatformView
 import io.github.durun.vst3kotlin.hosting.Module
-import io.github.durun.vst3kotlin.hosting.PluginInstance
 import io.github.durun.window.Window
 import io.github.durun.window.WindowClass
 import platform.windows.WM_QUIT
 import kotlin.test.Test
 
 class WindowTest {
-    @Test
+    //@Test
     fun window() {
         val window = Window.create("Hello", WindowClass.Simple)
         window.resize(480, 320)
@@ -24,7 +22,7 @@ class WindowTest {
         } while (msg.message.toInt() != WM_QUIT)
     }
 
-    @Test
+    //@Test
     fun vstWindow() {
         val path = Path.of("C:\\Program Files\\Common Files\\VST3\\FabFilter Pro-Q 3.vst3") //testResources.resolve("vst3/again.vst3")
 
@@ -32,21 +30,19 @@ class WindowTest {
         val window = Window.create("VST3", WindowClass.Vst(null))
 
         Module.of(path).use { module ->
-            val clazz = module.factory.classInfo.find { it.category == VstClassCategory.AudioEffect }
-                ?: throw NoSuchElementException("no Audio Effect Class")
-            PluginInstance.create(module.factory, clazz.classId).use {
-                it.plugView?.apply {
-                    println(size)
-                    val width = size.right - size.left
-                    val height = size.bottom - size.top
-                    window.resize(width, height)
-                    println("resized: $width*$height")
-                    attached(PlatformView(window.hwnd), "HWND")
-                    println("attached.")
-                } ?: println("No PlugView.")
-                window.show()
-                loop()
-            }
+            module.classes.filter { it.info.category == VstClassCategory.AudioEffect }
+                .map { it.createInstance() }.forEach {instance->
+                    instance.plugView?.apply {
+                        val width = size.right - size.left
+                        val height = size.bottom - size.top
+                        window.resize(width, height)
+                        println("resized: $width*$height")
+                        attached(PlatformView(window.hwnd), "HWND")
+                        println("attached.")
+                    } ?: println("No PlugView.")
+                    window.show()
+                    loop()
+                }
         }
 
     }
