4ec73ff078dfce179529c74c7e27a5ea8570484a
Durun <NegiIgaiNuki@gmail.com>
2021-03-01 15:12:37 +0900

    refactor

--- interop/src/nativeMain/kotlin/io/github/durun/vst3kotlin/hosting/ControllerInstance.kt
+++ interop/src/nativeMain/kotlin/io/github/durun/vst3kotlin/hosting/ControllerInstance.kt
@@ -6,6 +6,7 @@ import io.github.durun.vst3kotlin.base.PluginFactory
 import io.github.durun.vst3kotlin.base.UID
 import io.github.durun.vst3kotlin.cppinterface.HostCallback
 import io.github.durun.vst3kotlin.gui.PlugView
+import io.github.durun.vst3kotlin.gui.ViewType
 import io.github.durun.vst3kotlin.vst.Component
 import io.github.durun.vst3kotlin.vst.ComponentHandler
 import io.github.durun.vst3kotlin.vst.EditController
@@ -21,12 +22,12 @@ class ControllerInstance(
 
 		fun create(from: PluginFactory, classID: UID, hostContext: HostCallback = HostCallback): ControllerInstance {
 
-			val component: Component = runCatching { from.createComponent(classID) }
+			val component: Component = from.runCatching { createComponent(classID) }
 				.onSuccess { log.info { "Created component" } }
 				.onFailure { log.error { "Failed to create component" } }
 				.getOrThrow()
 
-			val controller: EditController = runCatching { component.queryEditController() }
+			val controller: EditController = component.runCatching { queryEditController() }
 				.onSuccess { log.info { "Queried EditController" } }
 				.onFailure { log.warn { "Failed to query EditController. Try create instead." } }
 				.recoverCatching { from.createEditController(component.controllerClassID) }
@@ -36,32 +37,28 @@ class ControllerInstance(
 
 			/** Created **/
 
-			runCatching { component.initialize(hostContext) }
+			component.runCatching { initialize(hostContext) }
 				.onSuccess { log.info { "Initialized component" } }
 				.onFailure { log.error { "Failed to initialize component" } }
-				.getOrThrow()
-			runCatching { controller.setComponentHandler(ComponentHandler(hostContext.ptr)) }
+
+			controller.runCatching { setComponentHandler(ComponentHandler(hostContext.ptr)) }
 				.onSuccess { log.info { "Set Callback as ComponentHandler to EditController" } }
 				.onFailure { log.warn { "Failed to set Callback to EditController" } }
-				.getOrThrow()
-
-			controller.runCatching { initialize(hostContext) }
+				.mapCatching { controller.initialize(hostContext) }
 				.onSuccess { log.info { "Initialized EditController" } }
 				.onFailure { log.warn { "Failed to create EditController" } }
-				.getOrThrow()
 
 			/** Initialized **/
 
 			runCatching { component.connectEach(controller) }
 				.onSuccess { log.info { "Connected Component and EditController" } }
 				.onFailure { log.error { "Failed to connect Component and EditController" } }
-				.getOrThrow()
 
 			/** Connected **/
 
 			val platformType = "HWND" // TODO: type for multiplatform
 			val plugView = runCatching {
-				val view: PlugView? = null //controller.createView(ViewType.Editor)
+				val view: PlugView? = controller.createView(ViewType.Editor)
 				checkNotNull(view)
 			}
 				.onSuccess { log.info { "Created PlugView" } }
