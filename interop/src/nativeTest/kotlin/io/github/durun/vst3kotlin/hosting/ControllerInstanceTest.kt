a6d5122f5f7ed1c66df21d9c74ed5d7579e3a00d
Durun <negiigainuki@gmail.com>
2021-03-01 14:18:22 +0900

    add test

---
+++ interop/src/nativeTest/kotlin/io/github/durun/vst3kotlin/hosting/ControllerInstanceTest.kt
@@ -0,0 +1,24 @@
+package io.github.durun.vst3kotlin.hosting
+
+import io.github.durun.io.use
+import io.github.durun.vst3kotlin.base.VstClassCategory
+import io.github.durun.vst3kotlin.testResources
+import kotlin.test.Test
+
+class ControllerInstanceTest {
+
+	val path = testResources.resolve("vst3/again.vst3")
+
+	@Test
+	fun test() {
+		Module.of(path).use { module ->
+			println("Module open.")
+			val classInfo = module.factory.classInfo.first { it.category == VstClassCategory.AudioEffect }
+			ControllerInstance.create(module.factory, classInfo.classId).use {
+				println("ControllerInstance open.")
+			}
+			println("ControllerInstance closed.")
+		}
+		println("Module closed.")
+	}
+}
