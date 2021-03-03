67ef8132526b84d5f4dc72ecdebe8155eeec709d
Durun <negiigainuki@gmail.com>
2021-03-03 15:09:52 +0900

    refactor build script

--- interop/build.gradle.kts
+++ interop/build.gradle.kts
@@ -129,35 +129,55 @@ tasks { // for testing
         os.isLinux -> "linuxX64"
         else -> throw GradleException("${os.familyName} is not supported.")
     }
-    val zipName = "vst3samples-$targetName.zip"
-    val zipPath = buildDir.resolve(zipName)
-    val downloadSamples by creating(Download::class) {
-        src("https://github.com/Durun/vst3experiment/releases/download/samples/$zipName")
-        dest(zipPath)
+    val vst3sdkSamples by when {
+        os.isWindows -> creatingDownloadZip(
+            url = "https://github.com/Durun/vst3experiment/releases/download/samples/vst3samples-windowsX64.zip",
+            md5 = "0f2d5aa9999f2fd40270bafa459502c4",
+            dest = projectDir.resolve("src/windowsX64Test/resources/vst3"),
+            fromDepth = 2
+        )
+        os.isMacOsX -> creatingDownloadZip(
+            url = "https://github.com/Durun/vst3experiment/releases/download/samples/vst3samples-macosX64.zip",
+            md5 = "b143d6fd79cbf345212f08a9d0659d57",
+            dest = projectDir.resolve("src/macosX64Test/resources/vst3"),
+            fromDepth = 2
+        )
+        os.isLinux -> creatingDownloadZip(
+            url = "https://github.com/Durun/vst3experiment/releases/download/samples/vst3samples-linuxX64.zip",
+            md5 = "7762726a6da2d2b2bd34ac54c7184dda",
+            dest = projectDir.resolve("src/linuxX64Test/resources/vst3"),
+            fromDepth = 2
+        )
+        else -> throw GradleException("${os.familyName} is not supported.")
     }
-    val verifySamples by creating(Verify::class) {
-        dependsOn(downloadSamples)
-        src(zipPath)
+    getByName("${targetName}Test") {
+        dependsOn(vst3sdkSamples)
+    }
+}
+
+fun TaskContainerScope.creatingDownloadZip(url: String, md5: String, dest: File, fromDepth: Int = 0) = creating {
+    val download = create<Download>("${name}Download") {
+        src(url)
+        dest(temporaryDir)
+    }
+    val outputFile = download.outputFiles.single()
+    val verify = create<Verify>("${name}Verify") {
+        dependsOn(download)
+        src(outputFile)
         algorithm("MD5")
-        when (zipName) {
-            "vst3samples-linuxX64.zip" -> checksum("7762726a6da2d2b2bd34ac54c7184dda")
-            "vst3samples-windowsX64.zip" -> checksum("0f2d5aa9999f2fd40270bafa459502c4")
-            "vst3samples-macosX64.zip" -> checksum("b143d6fd79cbf345212f08a9d0659d57")
-        }
+        checksum(md5)
     }
-    val unzipSamples by creating(Copy::class) {
-        dependsOn(verifySamples)
-        from(zipTree(zipPath))
-        val destRoot = projectDir.resolve("src/${targetName}Test/resources/vst3")
+    val unzip = create<Copy>("${name}Unzip") {
+        dependsOn(verify)
+        from(zipTree(outputFile))
+        val destRoot = projectDir.resolve(dest)
         into(destRoot)
         eachFile {
             val dest =
-                if (2 < relativePath.segments.size) relativePath.segments.drop(2).joinToString("/")
+                if (fromDepth < relativePath.segments.size) relativePath.segments.drop(fromDepth).joinToString("/")
                 else relativePath.pathString
             copyTo(destRoot.resolve(dest))
         }
     }
-    getByName("${targetName}Test") {
-        dependsOn(unzipSamples)
-    }
+    dependsOn(unzip)
 }
