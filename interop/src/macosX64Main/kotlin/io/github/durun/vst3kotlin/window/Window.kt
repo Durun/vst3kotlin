68fd6992a60da8ff3c093e88e587dd0fe7f96dc4
Durun <negiigainuki@gmail.com>
2021-03-03 18:24:40 +0900

    add skelton implement for MacOS

---
+++ interop/src/macosX64Main/kotlin/io/github/durun/vst3kotlin/window/Window.kt
@@ -0,0 +1,21 @@
+package io.github.durun.vst3kotlin.window
+
+import io.github.durun.data.Vec2
+import io.github.durun.data.toUInt
+import io.github.durun.util.logger
+import kotlinx.cinterop.*
+import platform.posix.usleep
+import x11.*
+
+actual class Window {
+	actual val ptr: COpaquePointer TODO()
+	actual fun show() = TODO()
+	actual fun loop(continueNext: (WindowEvent) -> Boolean) = TODO()
+	actual fun resize(size: Vec2<Int>) = TODO()
+
+	actual companion object {
+		private val log by logger()
+		actual val platformType: String = "HIView"
+		actual fun create(size: Vec2<Int>, name: String): Window = TODO()
+	}
+}
